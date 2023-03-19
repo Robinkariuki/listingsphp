3ac953f79af400311bb052ffc2fac2440579a3a2
unknown <robinkaris7@gmail.com>
2023-03-19 11:53:33 +0300

    added serach functionality

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,19 @@ use App\Models\listing;
 |
 */
 
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing  
+
+
 
 Route::get('/',[ListingController::class,'index']);
 
 // single listing
 
-Route::get('/listings/{listing}',[ListingController::class,'show']);
+Route::get('/listings/{listing}',[ListingController::class,'show']); 
